609e1f9976ee3ed5e843b62f515492b36454fb7c
Brian <brianlin1590@gmail.com>
2021-10-29 15:46:31 -0400

    finished anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -29,6 +29,20 @@ end
 # p second_anagram?("abc", "cba") #true
 # p second_anagram?("twooo","words") #false
 
+def third_anagram?(str_1, str_2)
+    str_1.chars.sort == str_2.chars.sort # O(nlogn)
+end
+# p third_anagram?("abc", "cba") #true
+# p third_anagram?("twooo","words") #false
+
+def fourth_anagram_bonus?(str_1, str_2)
+    hash = Hash.new {|h,k| h[k] = [0,0]}
+    str_1.each_char {|char| hash[char][0]+=1} #n
+    str_2.each_char {|char| hash[char][1]+=1} #n
+    hash.each_value.all? {|arr| arr[0]==arr[1]} #n
+end #O(n)
+# p fourth_anagram_bonus?("abc", "cba") #true
+# p fourth_anagram_bonus?("twooo","words") #false
 
 
 
