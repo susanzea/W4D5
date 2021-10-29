290ddc87a483aa2d88fc261cb16849a61df842de
Brian <brianlin1590@gmail.com>
2021-10-29 15:05:11 -0400

    finished phase 2 of largest_contiguous_subsum

--- exec_time_diffs.rb
+++ exec_time_diffs.rb
@@ -14,8 +14,8 @@ def my_min(list)
     min
 end
 
-# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min(list)  # =>  -5
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)  # =>  -5
 
 def my_min_fast(list)
     min = list.first
@@ -40,11 +40,31 @@ def largest_contiguous_subsum(list)
         end
     end
 
-    sub_arrs.map! { |sub| sub.sum }    #these two lines are 2n
-    sub_arrs.max
+    sum_arr = sub_arrs.map { |sub| sub.sum }    #these two lines are 2n
+    sum_arr.max
 end                                        #//final complexity is O(n^2)
 
 list = [5, 3, -7]
-p largest_contiguous_subsum(list) # => 8
+list2 =[-5,-3,-8,-2]
+list3 =[2,-1,-1,4,-2]
+# p largest_contiguous_subsum(list) # => 8
 
+def largest_contiguous_subsum_fast(list)
+    largest_sum = 0
+    curr_sum = 0
+    return list.max if list.all? {|num| num < 0}            #O(n)
+    list.each do |num|                                      #O(n)
+        if num + curr_sum <= 0
+            curr_sum = 0
+            next
+        end
+        curr_sum += num
+        largest_sum = curr_sum if curr_sum > largest_sum
+    end
+    largest_sum
+end                                                        #O(2n) => O(n)
+
+p largest_contiguous_subsum_fast(list)
+p largest_contiguous_subsum_fast(list2)
+p largest_contiguous_subsum_fast(list3)
 
